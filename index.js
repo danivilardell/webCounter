c1fd74110725e56d8d8f77e94821ff4e2b3764ff
Daniel Vilardell Regué <danielvilardellregue@Daniels-MacBook-Pro.local>
2022-04-09 13:47:01 +0200

    testing autodeployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+
 const express = require("express");
 var path = require('path');
 const app = express();
@@ -6,10 +6,9 @@ var public = path.join(__dirname, 'public');
 app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
 app.use('/', express.static(public));
 
-app.listen(process.env.PORT, ()=>console.log("Listening on http port 8081"))
+//app.listen(process.env.PORT)
+app.listen(8081, ()=>console.log("Listening on http port 8081"))
 const websocketServer = require("websocket").server;
-const httpServer = http.createServer();
-httpServer.listen(8080, () => console.log("Listening on 8080"));
 
 const clients = {};
 const counters = {};
